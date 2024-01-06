75c054b3531e39178b747b7213dce4f70aa61965
Rajdeep <snnbarot@gmail.com>
2024-01-06 16:34:36 +0530

    Updated score of students

--- components/Achievements.vue
+++ components/Achievements.vue
@@ -1,63 +1,63 @@
 <script setup>
 const experts = [
   {
-    name: "JASHPREET BHULLAR",
-    description: "O2 IAS Academy",
-    image: "/favicon.ico",
+    score: "90/100",
+    name: "Shaili Parekh",
+    id: 1,
   },
   {
-    name: "Er. Nidhi Pandey",
-    description: "Seyago Shiksha",
-    image: "/favicon.ico",
+    score: "88/100",
+    name: "Astha Patel",
+    id: 2,
   },
   {
-    name: "SAGAR KUMAR",
-    description: "SI COMPUTER ZONE",
-    image: "/favicon.ico",
+    score: "79/100",
+    name: "Nikul Chaudhary",
+    id: 3,
   },
   {
-    name: "Nainesh Nagekar",
-    description: "O2 IAS Academy",
-    image: "/favicon.ico",
+    score: "76/100",
+    name: "Karan Parmar",
+    id: 4,
   },
   {
-    name: "Anil Prajapati",
-    description: "Seyago Shiksha",
-    image: "/favicon.ico",
-  },
-  {
-    name: "Hitesh Verma",
-    description: "SI COMPUTER ZONE",
-    image: "/favicon.ico",
+    score: "76/100",
+    name: "Soham Patel",
+    id: 5,
   },
 ];
 </script>
 
 <template>
-  <div
-    class="flex flex-col items-center justify-around gap-4  to-accent p-6 py-28 md:flex-row bg-primary"
-  >
-    <h2 class="text-4xl font-bold text-bkg">Achievements By Our Experts</h2>
-    <div class="flex flex-col md:flex-row space-x-6">
-      <div
-        v-for="expert in experts"
-        :key="expert.name"
-        class="flex flex-col items-center"
-      >
-        <img
-          :src="expert.image"
-          alt="Expert Image"
-          class="h-16 w-16 rounded-full border-2 border-white object-cover"
-        />
-        <h3 class="mt-2 text-white">{{ expert.name }}</h3>
-        <p class="text-sm text-white">{{ expert.description }}</p>
+  <div class="flex flex-col items-center justify-evenly gap-4  to-accent px-6 py-28 md:flex-row bg-primary">
+    <h2 class="text-4xl font-bold text-bkg">Achievements By Our Students</h2>
+    <div class="flex ">
+      <div v-for="expert in experts" :key="expert.name" class="flex flex-col items-center m-2">
+        <img src="/favicon.png" alt="Student Image" class="h-16 w-16 rounded-full border-2 border-white object-cover" />
+        <h3 class="text-white text-center mt-2">{{ expert.score }} <br> <span class="text-sm text-white">{{ expert.name
+        }}</span></h3>
       </div>
     </div>
-    <!-- <div class="flex space-x-2">
-      <button class="text-3xl text-white">&lt;</button>
-      <button class="text-3xl text-white">&gt;</button>
-    </div> -->
   </div>
 </template>
 
 <style scoped></style>
+
+<!-- <div class="overflow-x-auto">
+  <table class="table table-zebra">
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Score</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="expert in experts" :key="expert.id">
+        <th>{{ expert.id }}</th>
+        <td>{{ expert.name }}n</td>
+        <td>{{ expert.score }}</td>
+      </tr>
+    </tbody>
+  </table>
+</div> -->
