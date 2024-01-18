37b2d64417b331f79811d2c0c5a5a0498c1db309
Rajdeep <snnbarot@gmail.com>
2024-01-18 17:49:48 +0530

    Updated depandancies

--- server/api/submitsheet.js
+++ server/api/submitsheet.js
@@ -1,43 +1,40 @@
 import { google } from 'googleapis';
 
-export default defineEventHandler (async event => {
-    try {
-        // Extract request parameters
-        // Hardcoded right now
-        const formData = [
-          "Rajdeep",
-          "7878787878",
-          "rajdeepbarot23@gmail.com",
-          "Vercel App"
-        ]
-    
-        // Authenticate with Google Sheets
-        const auth = new google.auth.GoogleAuth({
-          keyFile: "../credentials.json", // Ensure credentials.json is present in the function directory
-          scopes: "https://www.googleapis.com/auth/spreadsheets",
-        });
-        const client = await auth.getClient();
-        const googleSheets = google.sheets({ version: "v4", auth: client });
-        const spreadsheetId = "12_6AF5KehrIbbwsDkc4WNA9CVfg-mwGcBJgsoo5kRdI";
-    
-        // Append data to Google Sheets
-        await googleSheets.spreadsheets.values.append({
-          auth,
-          spreadsheetId,
-          range: "DemoClass!A:D",
-          valueInputOption: "USER_ENTERED",
-          resource: {
-            values: [formData],
-          },
-        });
-    
-        res.status(200).send("Success");
-      } catch (error) {
-        console.error(error);
-        res.status(500).send("Error");
-      }
+export default defineEventHandler(async (event) => {
+  try {
+    // Extract request parameters
+    const query = getQuery(event) 
+    const formData = [
+      query.name,
+      query.phone,
+      query.email,
+      query.city || query.message,
+    ]
 
-    return {
-        
-    }
-})
+    // Authenticate with Google Sheets
+    const auth = new google.auth.GoogleAuth({
+      keyFile: "credentials.json", // Ensure credentials.json is present in the function directory
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    const spreadsheetId = "12_6AF5KehrIbbwsDkc4WNA9CVfg-mwGcBJgsoo5kRdI";
+    const range = query.sheet; 
+    // Append data to Google Sheets
+    await googleSheets.spreadsheets.values.append({
+      auth,
+      spreadsheetId,
+      range,
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [formData],
+      },
+    });
+
+    return "Success"
+  } catch (error) {
+    console.error(error);
+    return "Error"
+  }
+}
+)
