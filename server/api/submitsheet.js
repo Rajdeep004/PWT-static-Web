d65673fb15d52c4eb67b1d10141e8ccd2c020e1c
Rajdeep <snnbarot@gmail.com>
2024-02-27 18:55:56 +0530

    fix: vercel allocation memory

--- server/api/submitsheet.js
+++ server/api/submitsheet.js
@@ -1,22 +1,22 @@
 import { google } from 'googleapis';
 
 const runtimeConfig = useRuntimeConfig()
-// Authenticate with Google Sheets
-const auth = new google.auth.GoogleAuth({
-  credentials: {
-    client_email: runtimeConfig.GOOGLE_CLIENT_EMAIL,
-    private_key: runtimeConfig.GOOGLE_PRIVATE_KEY,
-  },
-  scopes: "https://www.googleapis.com/auth/spreadsheets",
-});
-const client = await auth.getClient();
-const googleSheets = google.sheets({ version: "v4", auth: client });
-const spreadsheetId = "12_6AF5KehrIbbwsDkc4WNA9CVfg-mwGcBJgsoo5kRdI";
 
 export default defineEventHandler(async (event) => {
   try {
+    // Authenticate with Google Sheets
+    const auth = new google.auth.GoogleAuth({
+      credentials: {
+        client_email: runtimeConfig.GOOGLE_CLIENT_EMAIL,
+        private_key: runtimeConfig.GOOGLE_PRIVATE_KEY,
+      },
+      scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+    const client = await auth.getClient();
+    const googleSheets = google.sheets({ version: "v4", auth: client });
+    const spreadsheetId = "12_6AF5KehrIbbwsDkc4WNA9CVfg-mwGcBJgsoo5kRdI";
     // Extract request parameters
-    const query = getQuery(event) 
+    const query = getQuery(event)
     const formData = [
       query.name,
       query.phone,
@@ -24,8 +24,8 @@ export default defineEventHandler(async (event) => {
       query.city || query.message,
     ]
 
-    
-    const range = query.sheet; 
+
+    const range = query.sheet;
     // Append data to Google Sheets
     await googleSheets.spreadsheets.values.append({
       auth,
