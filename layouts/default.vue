220b59e718b0733428a5585ba3d611b49679be68
Rajdeep <snnbarot@gmail.com>
2024-02-27 17:36:29 +0530

    added vercel analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+
 import { ref } from 'vue'
 import { Dialog, DialogPanel } from '@headlessui/vue'
 
@@ -13,6 +15,7 @@ const mobileMenuOpen = ref(false)
 </script>
 
 <template>
+  <SpeedInsights />
   <header class="">
     <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
