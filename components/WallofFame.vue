30a1616cff63c4300fae43552dbbf945606fc24b
Rajdeep <snnbarot@gmail.com>
2024-03-22 10:36:20 +0530

    Added Refinements

---
+++ components/WallofFame.vue
@@ -0,0 +1,46 @@
+<script setup>
+const RESULT_COUNT = 12;
+
+const images = [];
+for (let i=1; i <= RESULT_COUNT; i++){
+    images.push(`/img/WOF/${i}.png`)
+}
+</script>
+
+<template>
+    <section class="container mx-auto">
+        <h2 class="h2 text-center mb-8">Wall of Fame</h2>
+        <Swiper 
+            :modules="[SwiperAutoplay, SwiperNavigation, SwiperPagination]"
+            :space-between="24" 
+            :slides-per-view="4" 
+            :loop="true" 
+            :autoplay="{
+                delay: 2000,
+                disableOnInteraction: false,
+            }"
+            :pagination="{
+               el: '#swiper-pagination',
+               clickable: true
+            }"
+            :navigation="{
+                nextEl: '.next',
+                prevEl: '.prev'
+            }"
+            class=" max-w-6xl relative"
+        >
+            <Icon name="tabler:square-rounded-chevron-left-filled" class="text-black prev text-4xl" />
+            <SwiperSlide v-for="path in images" :key="path">
+                <img 
+                    :src="path" 
+                    alt="Result"
+                    class="shadow-lg"
+                />
+            </SwiperSlide> 
+            <Icon name="tabler:square-rounded-chevron-right-filled" class="text-black next text-4xl" />  
+        </Swiper>
+        <div id="swiper-pagination"/>
+    </section>
+</template>
+
+<style scoped lang="postcss"></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,12 @@
-<script setup></script>
-
 <template class="min-h-screen">  
   <NuxtLink class="btn btn-primary fixed z-10 end-0 -rotate-90 hidden md:inline-flex -mx-8 bottom-48 !capitalize" to="/inquiry"> Inquiry</NuxtLink>
-  <Hero class="container my-24"/>
+  <Inquiry />
+  <Hero class="container my-"/>
   <Demo class="!mx-0 my-12"/>
-  <Services class="container my-12"/>
+  <Services class="container my-20 mx-auto"/>
+  <!-- <WallofFame class="container mx-auto my-28"/> -->
   <FacultyStudent class="container my-12"/>
   <InfographicFlow class="container my-12"/>
   <Achievements class="!mx-0 my-12"/>
   <FAQs class="container my-12"/>
 </template>
-
-<style scoped lang="postcss"></style>
